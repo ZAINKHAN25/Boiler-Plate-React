c0a0ab97fe00032e15a752469ffc781f3775be8b
ZAIN-KHAN-25 <zainarfeen87@gmail.com>
2024-03-10 23:15:58 +0500

    New commit

---
+++ src/Components/Body/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Body() {
+    return (
+        <div>
+            body
+        </div>
+    )
+}
+
+export default Body;
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Navbar from "../../Components/Navbar/index.jsx";
+import Body from "../../Components/Body/index.jsx";
 import "./Home.css"
 
 function Home() {
   return (
-    <Navbar />
+    <>
+      <Navbar />
+      <Body />
+    </>
   )
 }
 
